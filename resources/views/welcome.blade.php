c53d3a56677e8280bb38f605f6fbd475a452935f
HerwinMardhana <herwin.mardhana@gmail.com>
2018-09-18 13:42:27 +0800

    Homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,9 +45,50 @@
               <hr class="my-4">
               <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
               <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-            </div>
+          </div>
+        </div><!--end of row-->
+        <div class="row">
+        <div class="col-md-8">
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a href="" class="btn btn-primary">Read More</a>
+          </div>
+          <hr>
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a href="" class="btn btn-primary">Read More</a>
+          </div>
+          <hr>
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a href="" class="btn btn-primary">Read More</a>
+          </div>
         </div>
+        <div class="col-md-3 offset-md-1">
+
+        </div>
+      </div>
       </div><!--end of container-->
+
+
    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
